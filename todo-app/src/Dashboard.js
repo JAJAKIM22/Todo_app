1c2ef87dcf8a64aaeb13e104ff49902e6a5ee90e
Peter Kimani <peterjajakim@gmail.com>
2022-08-23 08:56:22 +0300

    CREATED A DROPDOWN MENU

--- todo-app/src/Dashboard.js
+++ todo-app/src/Dashboard.js
@@ -4,13 +4,16 @@ import { Link } from "react-router-dom";
  export default function Dashboard(){
     return(
         <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+            </button>
         <nav>
-        <ul >
+        <ul class="dropdown-menu">
             <li>
-                <Link to="/homepage">HOMEPAGE</Link>
+                <Link to="/homepage"><button class="dropdown-item" type="button">HOMEPAGE</button></Link>
             </li>
             <li>
-                <Link to="/funpage">FUNPAGE</Link>
+                <Link to="/funpage"><button class="dropdown-item" type="button">FUNPAGE</button></Link>
             </li>
         </ul>
         </nav>
